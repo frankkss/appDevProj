26d8c98e7e431162d3d9ac7acaa825d9dff33a8f
Divino Franco R. Aurellano <diaurellano@my.cspc.edu.ph>
2024-12-02 19:09:12 +0800

    Add initial project structure with Flask setup and basic templates
    - Created Flask application with SQLAlchemy integration for database management.
    - Added models for items with attributes like name, price, barcode, and description.
    - Implemented basic routing for home, market, and login pages.
    - Added HTML templates for home, login, register, and market pages.
    - Included CSS for styling and a settings file for live server configuration.

--- app.py
+++ app.py
@@ -1,12 +1,28 @@
 from flask import Flask, render_template
-
+from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
+db = SQLAlchemy(app)
 
-@app.route("/")
-def home():
-    return render_template('index.html')
+class Item(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(length=30), nullable=False, unique=True)
+    price = db.Column(db.Integer(), nullable=False)
+    barcode = db.Column(db.String(length=12), nullable=False, unique=True)
+    description = db.Column(db.String(length=1024), nullable=False, unique=True)
 
+    def __repr__(self):
+        return f'Item {self.name}'
+    
+    
+@app.route('/')
+@app.route('/home')
+def home_page():
+    return render_template('home.html')
 
+@app.route('/market')
+def market_page():
+    return render_template('market.html')
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
