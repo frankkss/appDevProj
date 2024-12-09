ff879334e7a132cf963a65b4dc161ee4ba17ce8d
Divino Franco R. Aurellano <diaurellano@my.cspc.edu.ph>
2024-12-09 09:28:47 +0800

    Connect to Heroku Postgres

--- reliamed/__init__.py
+++ reliamed/__init__.py
@@ -1,7 +1,11 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+import os
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///reliamed.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 db = SQLAlchemy(app)
 
 from reliamed import routes
