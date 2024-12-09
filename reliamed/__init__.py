fdcd1fdae2c6f6c78f8b6cf1dfb73f4146a63db3
Divino Franco R. Aurellano <diaurellano@my.cspc.edu.ph>
2024-12-09 09:40:09 +0800

    Add Flask-Migrate

--- reliamed/__init__.py
+++ reliamed/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 import os
 
 app = Flask(__name__)
@@ -7,5 +8,6 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///reliamed.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 from reliamed import routes
