982abe609a80681b517c314ba0cda38592bcacb0
Divino Franco R. Aurellano <115411774+frankkss@users.noreply.github.com>
2024-12-23 03:43:45 +0000

    tweaked app for heroku

--- reliamed/__init__.py
+++ reliamed/__init__.py
@@ -8,7 +8,7 @@ import os
 
 app = Flask(__name__)
 # Use DATABASE_URL from environment variables if available, otherwise fallback to local sqlite
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'sqlite:///reliamed.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://uaqs6a9msldu91:p7ed62fe468cd91eb4450559b4782c731ec92a71dff37b1c54f29fd1091ff934a@cfls9h51f4i86c.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/d60vupoa4r6o39'
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'default_secret_key')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {'connect_args': {'timeout': 20}}  # Add this line
